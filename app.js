67218955b4abb5da0bbb63d6c9c599dd7965098b
tgtiburon <tg.tiburon@gmail.com>
2021-12-17 12:24:55 -0500

    fixed some queries

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const cTable = require('console.table');
 
 
 const init = () => {
+    // I don't think I need this since we are not running a server
+
+    db.connect(function(err) {
+        if (err) {
+            console.log(err);
+            
+        } else {
+            console.log('Connected');
+        }
+    })
 
    
         // db.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", 
@@ -26,6 +36,9 @@ const init = () => {
         console.log("--------------------------------------------------------\x1b[37m");
         console.log("\n");
         
+        // I don't think I need this
+
+      
 
         mainMenu();
        
@@ -79,7 +92,7 @@ const mainMenu = () => {
                     updateAnEmployeeRole();
                     break;
             
-                default: // Quit
+                case "Quit": // Quit
                 console.log("Thank you for using Staff Examiner!");
                 db.end();
                     break;
@@ -89,57 +102,293 @@ const mainMenu = () => {
 }
 
 const viewAllDepartments = () => {
-    console.log("Inside viewAllDepartments");
+   
+    const sql = `SELECT * FROM staff.department`;
 
-
-    const sql = `SELECT * FROM staff.departments`;
-
-    db.query(sql, (err, rows) => {
-        if(err) {
+    db.promise().query(sql)
+        .then(([ rows ])=> {
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            console.log('All Departments')
+            console.log('\n');
+            console.table(rows);
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            mainMenu();
+        })
+        .catch(err => {
             console.log(err);
-        }
-        console.log("\n");
-        console.table(rows);
-    });
-    mainMenu();
+        });
 
-}
+};
 
 const viewAllRoles = () => {
-    console.log("Inside viewAllRoles");
-    mainMenu();
+   // const sql = `SELECT * FROM staff.role`;
+   const sql = `SELECT role.id,
+                       role.title,
+                       department.name AS department, 
+                       role.salary
+                       FROM role
+                       LEFT JOIN department ON role.department_id = department.id; 
+                        `
+
+    db.promise().query(sql)
+        .then(([ rows ])=> {
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            console.log('All Roles')
+            console.log('\n');
+            console.table(rows);
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            mainMenu();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 
 }
 
 const viewAllEmployees = () => {
-    console.log("Inside viewAllEmployees");
-    mainMenu();
+
+    // INNER JOIN when both tables have all rows
+    // LEFT join employees and managers because not all employees have managers
+    const sql = `SELECT employee.id,
+                        employee.first_name,
+                        employee.last_name,
+                        role.title,
+                        department.name,
+                        role.salary,
+                        CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee 
+                        INNER JOIN role ON employee.role_id = role.id
+                        INNER JOIN department ON role.department_id = department.id
+                        LEFT JOIN employee manager ON employee.manager_id = manager.id;`;
+
+    db.promise().query(sql)
+        .then(([ rows ])=> {
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            console.log('All Employees')
+            console.log('\n');
+            console.table(rows);
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            mainMenu();
+        })
+        .catch(err => {
+            console.log(err);
+        });
 
 }
 
 const addADepartment = () => {
-    console.log("Inside addADepartment");
-    mainMenu();
+
+   inquirer.prompt(
+       {
+          type: "input",
+          name: "deptName",
+          message: "What is the name of the department you want to add?",
+          validate: deptName => {
+              if(deptName){
+                  console.log("Name received.");
+                  return true;
+              }else {
+                  console.log("Please enter a department name.");
+                  return false;
+              }
+          }
+       }
+
+   )
+   .then(deptName => {
+       console.log("New dept name = ", deptName);
+       const sql = `INSERT INTO department (name) VALUES (?)`;
+        const params = [deptName.deptName];
+        db.promise().query(sql,params)
+        .then(([ rows ])=> {
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            console.log('Add a Department')
+            console.log('\n');
+            console.table(rows);
+            console.log('\x1b[92m-----------------------\x1b[0m');
+            mainMenu();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+
+   });
+ 
 
 }
 
 const addARole = () => {
-    console.log("Inside addARole");
-    mainMenu();
 
+//SELECT * FROM roles; SELECT * FROM departments
+    const sql = `SELECT * FROM role`;
+    db.promise().query(sql)
+        .then(( [ rows ]) => {
+           // console.log("Below is rows");
+           // console.log(rows);
+           // console.table(rows);
+           console.log("rows");
+           console.log(rows);
+           const tmpRoles = rows.roleTitle;
+           console.table(tmpRoles);
+           console.log("in db.promise");
+
+           inquirer.prompt([
+            {
+               type: "input",
+               name: "roleTitle",
+               message: "What is the title of the role you want to add?",
+               validate: roleTitle => {
+                   if(roleTitle){
+                       console.log("Role received.");
+                       return true;
+                   }else {
+                       console.log("Please enter a role  title.");
+                       return false;
+                   }
+               }
+            },
+            {
+                type: "input",
+                name: "roleSalary",
+                message: "What is the salary of the role you want to add?",
+                validate: roleSalary => {
+                    if(roleSalary){
+                        console.log("Role received.");
+                        return true;
+                    }else {
+                        console.log("Please enter a department name.");
+                        return false;
+                    }
+                }   
+            }
+            // {
+            //     type: "list",
+            //     name: "roleDeptID",
+            //     message: "What is the department id of the role you want to add?",
+            //    choices : tmpRoles
+            //    }   
+            
+     
+        ])
+        .then(answers => {
+            console.log("New role: ", answers);
+            const sql = `INSERT INTO role (title, salary, department_id) VALUES (?,?,?)`;
+             const params = [answers.roleTitle, answers.roleSalary, answers.roleDeptID];
+             db.promise().query(sql,params)
+             .then(([ rows ])=> {
+                 console.log('\x1b[92m-----------------------\x1b[0m');
+                 console.log('Add a Role')
+                 console.log('\n');
+                 console.table(rows);
+                 console.log('\x1b[92m-----------------------\x1b[0m');
+                 mainMenu();
+             })
+             .catch(err => {
+                 console.log(err);
+             });
+    
+        });
+
+
+        })
+
+        .catch(err=> {
+            console.log(err);
+        });
+
+
+
+   
 }
 
 const addAnEmployee = () => {
-    console.log("Inside addAnEmployee");
 
-    mainMenu();
+    inquirer.prompt([
+        {
+           type: "input",
+           name: "employeeFName",
+           message: "What is the first name of the employee?",
+           validate: employeeFName => {
+               if(employeeFName){
+                  
+                   return true;
+               }else {
+                   console.log("Please enter the first name of the employee.");
+                   return false;
+               }
+           }
+        },
+        {
+            type: "input",
+            name: "employeeLName",
+            message: "What is the last name of the employee?",
+            validate: employeeLName => {
+                if(employeeLName){
+                 
+                    return true;
+                }else {
+                    console.log("Please enter the last name of the employee.");
+                    return false;
+                }
+            }   
+        },
+        {
+            type: "input",
+            name: "employeeRoleID",
+            message: "What is the department id of the employee?",
+            validate: employeeRoleID => {
+                if(employeeRoleID){
+                   
+                    return true;
+                }else {
+                    console.log("Please enter the department id of the employee? ");
+                    return false;
+                }
+            }   
+        },
+        {
+            type: "input",
+            name: "employeeManagerID",
+            message: "What is the manager id of the employee?",
+            validate: employeeManagerID => {
+                if(employeeManagerID){
+                   
+                    return true;
+                }else {
+                    console.log("Please enter the manager id of the employee? ");
+                    return false;
+                }
+            }   
+        }
+ 
+    ])
+    .then(answers => {
+        console.log("New employee: ", answers);
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+         const params = [answers.employeeFName, answers.employeeLName, answers.employeeRoleID, answers.employeeManagerID];
+         db.promise().query(sql,params)
+         .then(([ rows ])=> {
+             console.log('\x1b[92m-----------------------\x1b[0m');
+             console.log('Add a Role')
+             console.log('\n');
+             console.table(rows);
+             console.log('\x1b[92m-----------------------\x1b[0m');
+             mainMenu();
+         })
+         .catch(err => {
+             console.log(err);
+         });
+
+    });
+   
 
 }
 
 const updateAnEmployeeRole = () => {
-    console.log("Inside updateAnEmployeeRole");
 
-    mainMenu();
+    
+   
+
 
 }
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,31 @@
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
 
-CREATE TABLE departments(
+CREATE TABLE department(
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL
 
 );
 
-CREATE TABLE roles(
+CREATE TABLE role(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT 
+    department_id INT,
+    CONSTRAINT fk_dept_id FOREIGN KEY (department_id)
+    REFERENCES department(id) ON DELETE SET NULL
 
 );
 
-CREATE TABLE employees(
+CREATE TABLE employee(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT
-    manager_id INT 
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id)
+    REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager_id FOREIGN KEY (manager_id)
+    REFERENCES employee(id) ON DELETE SET NULL
 );
